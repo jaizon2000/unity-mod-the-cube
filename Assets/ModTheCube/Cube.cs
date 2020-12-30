868da1cd8f0c6602f03c341bd71974c64007543b
Jehdi Aizon <jaizon2000@gmail.com>
2020-12-29 20:05:50 -0700

    rotation speed

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    
+    public float rotateSpeed = 10.0f;
+
     void Start()
     {
-        transform.position = new Vector3(3, 4, 1);
-        transform.localScale = Vector3.one * 1.3f;
-        
+        transform.position = new Vector3(1, 4, 1);
+        transform.localScale = Vector3.one * 1.6f;
+
         Material material = Renderer.material;
-        
+
         material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
     }
-    
+
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.Rotate(10.0f * Time.deltaTime * rotateSpeed, 3.0f * Time.deltaTime * rotateSpeed, 0.0f);
     }
 }
